7acf505eff128e8059dcad990e51b09c350f5402
Diogo Duarte <diogo.goncalo.duarte@gmail.com>
2025-07-22 19:26:37 +0100

    db and some route setup

--- index.js
+++ index.js
@@ -6,17 +6,45 @@ import pg from "pg";
 
 const app = express();
 const port = 3000;
+env.config();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
 const db = new pg.Client({
-    
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
 });
 
 
+app.get("/login", (req,res) => {
+    res.render("login.ejs");
+});
 
 
+app.get("/register", (req,res) => {
+    res.render("register.ejs");
+});
+
+
+app.get("/logout", (req,res) => {
+
+});
+
+
+app.get("/workouts", (req,res) => {
+
+});
 
 
 
